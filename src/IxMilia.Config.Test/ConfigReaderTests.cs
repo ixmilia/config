a118441200fea78e46c3062ea9fc17c677bb2e20
Brett V. Forsgren <brett.forsgren@outlook.com>
2016-10-20 11:12:03 -0700

    enable simple parsing

--- src/IxMilia.Config.Test/ConfigReaderTests.cs
+++ src/IxMilia.Config.Test/ConfigReaderTests.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) IxMilia.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using Xunit;
 
@@ -10,5 +8,32 @@ namespace IxMilia.Config.Test
 {
     public class ConfigReaderTests
     {
+        private IDictionary<string, string> Parse(string data)
+        {
+            var dict = new Dictionary<string, string>();
+            var lines = data.Trim().Split(new[] { '\n' }).Select(l => l.TrimEnd('\r')).ToArray();
+            dict.ParseConfig(lines);
+            return dict;
+        }
+
+        [Fact]
+        public void SimpleParseTest()
+        {
+            var dict = Parse(@"
+; comment
+rootValue = true
+
+[section]
+# some other comment
+key = value
+
+[section.deeperSection]
+key = value2
+");
+            Assert.Equal(3, dict.Keys.Count);
+            Assert.Equal("true", dict["rootValue"]);
+            Assert.Equal("value", dict["section.key"]);
+            Assert.Equal("value2", dict["section.deeperSection.key"]);
+        }
     }
 }
---
+++ src/IxMilia.Config/ConfigExtensions.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) IxMilia.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace IxMilia.Config
+{
+    public static class ConfigExtensions
+    {
+        private static char[] Separator = new[] { '=' };
+
+        public static void ParseConfig(this IDictionary<string, string> dictionary, params string[] lines)
+        {
+            var prefix = string.Empty;
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line) || line.StartsWith(";") || line.StartsWith("#"))
+                {
+                    // ignore blank lines and comments
+                    continue;
+                }
+
+                if (line.StartsWith("[") && line.EndsWith("]"))
+                {
+                    // new section
+                    prefix = line.Substring(1, line.Length - 2) + ".";
+                }
+                else
+                {
+                    // key/value pair
+                    var parts = line.Split(Separator, 2);
+                    var key = parts[0].Trim();
+                    var value = parts.Length == 2 ? parts[1].Trim() : null;
+                    if (value != null)
+                    {
+                        dictionary[prefix + key] = value;
+                    }
+                }
+            }
+        }
+    }
+}
