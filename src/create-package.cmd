f64625dfed41ab1cc69b599a7e11bf2cc9d2243f
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-09-13 16:31:25 -0700

    let SourceLink handle the commit hash embedding

--- src/create-package.cmd
+++ src/create-package.cmd
@@ -3,4 +3,4 @@
 set PROJECT=%~dp0IxMilia.Config\IxMilia.Config.csproj
 dotnet restore %PROJECT%
 if errorlevel 1 exit /b 1
-dotnet pack --configuration Release %PROJECT% /p:OfficialBuild=true
+dotnet pack --configuration Release %PROJECT%
