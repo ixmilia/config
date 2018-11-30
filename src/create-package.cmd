44fdbce7e547469d4faa777553c987b063ef021f
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-11-30 09:38:18 -0800

    build everything to `artifacts/` with parameterized version

--- src/create-package.cmd
+++ src/create-package.cmd
@@ -1,6 +1,14 @@
 @echo off
 
-set PROJECT=%~dp0IxMilia.Config\IxMilia.Config.csproj
-dotnet restore %PROJECT%
+set PROJECT_NAME=IxMilia.Config
+set CONFIGURATION=Release
+set PROJECT=%~dp0\%PROJECT_NAME%\%PROJECT_NAME%.csproj
+
+dotnet restore "%PROJECT%"
 if errorlevel 1 exit /b 1
-dotnet pack --configuration Release %PROJECT%
+
+dotnet build "%PROJECT%" --configuration %CONFIGURATION%
+if errorlevel 1 exit /b 1
+
+dotnet pack --no-restore --no-build --configuration %CONFIGURATION% "%PROJECT%"
+if errorlevel 1 exit /b 1
